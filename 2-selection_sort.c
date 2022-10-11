ec9201299f22f25b3e71a8ea7664d8eec2b4f5d3
Tom-254 <211mseeratiego@gmail.com>
2022-10-11 22:28:50 +0000

    0x1B. C - Sorting algorithms & Big O

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,39 +1,44 @@
 #include "sort.h"
 
 /**
- * selection_sort - Sorts an array of integers in acending order using
- * the selection sort algorithm.
- * @array: An array of integers to needs to be sorted.
- * @size: The size of the array.
+ * selection_sort - sorts an array using the selection sort algorithm
+ *
+ * @array: pointer to integer array
+ * @size: size of array
+ *
+ * Return: Nothing
  */
 
 void selection_sort(int *array, size_t size)
 {
-	int min;
-	size_t i;
-	size_t j;
-	size_t position;
+	int flag = 0, tmp = 0, place = 0;
+	size_t i, j;
 
-	if (array == NULL)
+	if (!array)
 		return;
 
 	for (i = 0; i < size; i++)
 	{
-		min = array[i];
-		position = i;
-
+		flag = 0;
+		tmp = 0;
 		for (j = i + 1; j < size; j++)
 		{
-			if (min > array[j])
+			if (!flag && array[j] < array[i])
 			{
-				position = j;
-				min = array[j];
+				tmp = array[j];
+				place = j;
+				flag = 1;
+			}
+			if (flag && array[j] < tmp)
+			{
+				tmp = array[j];
+				place = j;
 			}
 		}
-		if (position != i)
+		if (flag)
 		{
-			array[position] = array[i];
-			array[i] = min;
+			array[place] = array[i];
+			array[i] = tmp;
 			print_array(array, size);
 		}
 	}
--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,108 +1,76 @@
 #include "sort.h"
-#include <stdlib.h>
-void sort(int *array, size_t beg, size_t size, size_t);
-int check(int *array, size_t beg, size_t end);
-int swap(int *, size_t, size_t, size_t);
-
+#include <stdio.h>
 /**
- * quick_sort - Sorts an array of integers in asceding order using the
- * quick sort algorithm implementing the Lomuto partition scheme.
- * @array: Array of integers that need to be sorted.
- * @size: The size of the array to sort.
+ * quick_sort - sorts an array using the quick sort algorithm
+ *
+ * @array: pointer to integer array
+ * @size: size of integer array
+ *
+ * Return: Nothing
  */
 void quick_sort(int *array, size_t size)
 {
-	size_t beg;
-
-	beg = 0;
-	if (size < 2 || array == NULL)
+	if (!array || size < 2)
 		return;
-	size = size - 1;
-	sort(array, beg, size, size + 1);
+	realqs(array, 0, size - 1, size);
 }
-
 /**
- * sort - Does the actual sorting.
- * @array: Array of integers that need to be sorted.
- * @beg: The lower bound of the array partition.
- * @end: The upper bound of the array partion.
- * @og: The original size of the whole array. Used only for printing.
+ * realqs - recursive function for sorting array
+ *
+ * @array: pointer to integer array
+ * @start: starting index
+ * @end: ending index
+ * @size: size of the array
+ *
+ * Return: Nothing
  */
-void sort(int *array, size_t beg, size_t end, size_t og)
+void realqs(int *array, int start, int end, size_t size)
 {
-	size_t i;
-	size_t j;
-	int piv;
-	int flag;
-	int c;
+	int pivot;
 
-	piv = array[end];
-	j = beg;
-	i = j - 1;
-	flag = 0;
-
-	for (j = beg; j < end; j++)
+	if (start < end)
 	{
-		if (array[j] <= piv)
+		pivot = partition(array, start, end, size);
+		realqs(array, start, pivot - 1, size);
+		realqs(array, pivot + 1, end, size);
+	}
+}
+/**
+ * partition - partitions array, swaps values, and prints
+ *
+ * @array: pointer to integer array
+ * @start: starting index
+ * @end: ending index
+ * @size: size of the array
+ *
+ * Return: Nothing
+ */
+int partition(int *array, size_t start, size_t end, size_t size)
+{
+	size_t i = start - 1;
+	size_t j;
+	int tmp = 0;
+	int tmp2 = 0;
+	size_t place = 0;
+
+	for (j = start; j < end; j++)
+	{
+		if (array[j] < array[end])
 		{
 			i++;
-			flag = swap(array, j, i, og);
+			tmp = array[i];
+			array[i] = array[j];
+			array[j] = tmp;
+			if (array[j] != array[i])
+				print_array(array, size);
 		}
 	}
 	i++;
-	flag = swap(array, j, i, og);
-
-	if (flag == 0)
-	{
-		c = check(array, beg, end);
-		if (c == 1)
-			sort(array, beg, end - 1, og);
-	}
-	else
-	{
-		sort(array, beg, i, og);
-		sort(array, i, end, og);
-	}
-}
-
-/**
- * swap - Swaps the numbers in the array.
- * @array: Array of integers that need to be sorted.
- * @i: The lower bound of the array partition.
- * @j: The upper bound of the array partion.
- * @og: The original size of the whole array. Used only for printing.
- * Return: 1 when a swap happens. 0 otherwise.
- */
-int swap(int *array, size_t i, size_t j, size_t og)
-{
-	int tmp;
-
-	if (j != i)
-	{
-		tmp = array[i];
-		array[i] = array[j];
-		array[j] = tmp;
-		print_array(array, og);
-		return (1);
-	}
-	return (0);
-}
-
-/**
- * check - checks if the array portion is already sorted.
- * @array: Array of integers that need to be sorted.
- * @beg: The lower bound of the array partition.
- * @end: The upper bound of the array partion.
- * Return: 1 if the array is not sorted. 0 otherwise.
- */
-int check(int *array, size_t beg, size_t end)
-{
-	size_t i;
-
-	for (i = beg; i < end; i++)
-	{
-		if (array[i] > array[i + 1])
-			return (1);
-	}
-	return (0);
+	place = i;
+	tmp2 = array[i];
+	array[i] = array[end];
+	array[end] = tmp2;
+	if (array[j] != array[i])
+		print_array(array, size);
+	return (place);
 }
